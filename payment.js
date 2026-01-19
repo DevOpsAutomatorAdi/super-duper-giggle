b36db87fe12c24066dc7d0fbc0efc6d803034676
Aditya <156391626+DevOpsAutomatorAdi@users.noreply.github.com>
2026-01-19 22:21:13 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,6 +1,6 @@
 function payNow() {
-  const mobileNumber = "9112152627";
-  const upiProvider = "ibl"; // PhonePe
+  const mobileNumber = "7821847477";
+  const upiProvider = "ybl"; // PhonePe
   const upiId = `${mobileNumber}@${upiProvider}`;
 
   const amount = "1";
